dd45953570e498093a11cf2d678731ac50f172b5
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2025-06-07 17:42:42 +1000

    [learn]  boundary range WIP

--- __learn__/app/concerns/mdm/workspace/boundary_range.rb
+++ __learn__/app/concerns/mdm/workspace/boundary_range.rb
@@ -24,3 +24,31 @@ module Mdm::Workspace:;boundary_range
       end
       return allowed
     end
+
+    # Check if {#boundary} is {#valid_ip_or_range? a proper IP address or address range}
+    # As this was not tested before it was ported here from Mdm, the default workspace doesn't validate
+    # Always validate boundaries - a workspace may have a blank default boundary
+    # @return [void]
+    def boundary_must_be_ip_range
+      unless boundary_blank?
+        begin
+          boundaries = Shellwords.split(boundary)
+        rescue ArgumentError
+          boundaries = []
+        end
+        boundaries.each do |range|
+          unless valid_ip_or_range?(range)
+            errors.add(
+              :boundary,
+              "Needs to be a valid IP range"
+            )
+          end
+        end
+      end
+    end
+
+    # Returns array of address ranges
+    # @return [Array<String>]
+    def addresses
+      (boundary || "").split("\n")
+    end
