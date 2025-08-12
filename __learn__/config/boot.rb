9b0821c9e8d6f0fbc07eb3b555e9c219b252d84a
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2025-08-12 23:51:21 +1000

    [learn] config boot WIP

--- __learn__/config/boot.rb
+++ __learn__/config/boot.rb
@@ -4,3 +4,24 @@ GEMFILE_EXTENSIONS = [
   '.local',
   ''
 ]
+msfenv_real_pathname = Pathname.new(__FILE__).realpath
+root = msfenv_real_pathname.parent.parent
+unless ENV['BUNDLE_GEMFILE']
+  require 'pathname'
+  GEMFILE_EXTENSIONS.each do |extension|
+    extension_pathname = root.join("Gemfile#{extension}")
+    if extension_pathname.readable?
+      ENV['BUNDLE_GEMFILE'] = extension_pathname.to_path
+      break
+    end
+  end
+end
+
+begin
+  require 'bundler/setup'
+rescue LoadError => e 
+  $stderr.puts "[*] Bundler didn't load, but got this error:"
+  $stderr.puts = "  '#{e}'"
+  $stderr.puts "[*] Either reinstall or upgrade bundler"
+  exit(1)
+end 
